e2c278af1533ecd5dcf6b2ea9022e9a940194bb3
dogboydog <dogboydog@users.noreply.github.com>
2023-01-07 19:43:54 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.IO;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var original = File.ReadAllText("../../../Hello.yarn");
+        Console.WriteLine($"Before:\n{original}");
+        var tagsAdded = Yarn.Compiler.Utility.AddTagsToLines(original);
+        Console.WriteLine($"\nAfter:\n{tagsAdded}");
+    }
+}
